135b4c334f9cf76ad2b38ee4dc6d9ccff024d2b9
fhdswatt-beep <fhdswatt@gmail.com>
2026-08-24 05:53:51 +0300

    Enable approved reference React transform

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import referenceUiTransform from './reference-ui-transform.js';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [referenceUiTransform(), react()],
 });
