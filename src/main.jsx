eb488112fbfd36374db1a7d5c455c4e16639adab
fhdswatt-beep <fhdswatt@gmail.com>
2026-08-24 16:19:26 +0300

    Restore legacy app data access and remove secure session bridge

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { installSecureSessionBridge } from './secure-session-bridge.js';
-
-installSecureSessionBridge();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
