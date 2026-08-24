2ab10d130f34c7437a03537b42034075ddcc35ae
fhdswatt-beep <fhdswatt@gmail.com>
2026-08-24 16:33:40 +0300

    feat(ui): load native v2 design system

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import './native-ui-v2.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
