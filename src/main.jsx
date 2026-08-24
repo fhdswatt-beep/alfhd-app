91ef6f1768aa85d292af397682b6ccb047c93369
fhdswatt-beep <fhdswatt@gmail.com>
2026-08-24 17:07:28 +0300

    feat(ui): switch entire app to radical design system

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './native-ui-v2.css';
+import './radical-ui.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
