991f0693c63709f69de2a8cf05b91e8538aea5cc
fhdswatt-beep <fhdswatt@gmail.com>
2026-08-24 07:53:52 +0300

    Split desktop and native mobile preview shells

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import RadicalPrototype from './RadicalPrototype.jsx';
+import MobilePrototype from './MobilePrototype.jsx';
+
+function PreviewRoot(){
+  const [mobile,setMobile]=useState(()=>typeof window!=='undefined'&&window.matchMedia('(max-width: 760px)').matches);
+  useEffect(()=>{
+    const mq=window.matchMedia('(max-width: 760px)');
+    const onChange=e=>setMobile(e.matches);
+    mq.addEventListener?.('change',onChange);
+    return()=>mq.removeEventListener?.('change',onChange);
+  },[]);
+  return mobile?<MobilePrototype/>:<RadicalPrototype/>;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RadicalPrototype />
+    <PreviewRoot />
   </React.StrictMode>
 );
